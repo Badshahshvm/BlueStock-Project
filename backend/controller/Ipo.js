9383e629d0b52ff81f11bb8f744517dd50231b85
Shivam Kumar <115875580+Badshahshvm@users.noreply.github.com>
2025-01-12 17:18:34 +0530

    Create Ipo.js

---
+++ backend/controller/Ipo.js
@@ -0,0 +1,30 @@
+
+
+  const addIpo=async(req,res)=>
+    {
+      try
+    {
+      
+    }
+    catch(err)
+    {
+      res.json({
+        success:false,
+        message:err.message})
+    }
+    }
+const upcommingIpo=async(req,res)=>
+  {
+try
+    {
+      
+    }
+    catch(err)
+    {
+      res.json({
+        success:false,
+        message:err.message})
+    }
+  }
+
+module.exports={addIpo,upcommingIpo};
