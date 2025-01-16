5f03aed4db564a4c959c8f7e918fe567ac71ce4a
Shivam Kumar <115875580+Badshahshvm@users.noreply.github.com>
2025-01-16 17:12:22 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,20 @@
-const express = require("express")
-const server = express()
-
-const port = 2025
-
-server.get('/signin', (req, res)=> {
-    res.send("SignIn Succesful")
-})
-
-server.listen(port, ()=> {
-  console.log("server started and running succesfully!")
+const express = require("express");
+const mongoose = require("mongoose");
+const fileUpload = require("express-fileupload")
+require("dotenv").config();
+const userRoute = require("./route/user")
+const ipoRoute = require("./route/ipo")
+const app = express();
+mongoose.connect(process.env.MONGOURL).then(() => console.log("Connected To The databse")).catch((err) => console.log("not connected"))
+app.use(express.json())
+app.use(
+              fileUpload({
+                            useTempFiles: true,
+                            tempFileDir: "/tmp/",
+              })
+);
+app.use("/api/v1/user", userRoute)
+app.use("/api/v1/ipo", ipoRoute)
+app.listen(process.env.PORT, () => {
+              console.log("server is running")
 })
