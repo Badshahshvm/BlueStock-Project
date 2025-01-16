ad52bbb7ab8fbef9d37c5ecd50c886368862e500
Shivam Kumar <115875580+Badshahshvm@users.noreply.github.com>
2025-01-16 17:06:53 +0530

    Update user.js

--- backend/model/user.js
+++ backend/model/user.js
@@ -1,6 +1,38 @@
-  const mongoose=require("mongoose");
-const userSchema=new mongoose.Schema({
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema({
+              firstname: {
+                            type: String,
+                            required: true
+              },
+              lastname:
+              {
+                            type: String,
+                            required: true,
+              },
+              email: {
+                            type: String,
+                            required: true,
+                            unique: true,
+              },
+              forgot_password_otp:
+              {
+                            type: String, default: null
+              },
+              forgot_password_expire: {
+                            type: Date,
+                            default: ""
+
+              },
+              role:
+              {
+                            type: String,
+                            enum: ["Admin", "User"],
+                            default: "User"
+              },
+              appliedIPOs: [{ type: mongoose.Schema.Types.ObjectId, ref: "IPO" }],
+
+
 })
 
-const userModel=mongoose.model("User",userSchema);
-module.exports=userModel;
+const userModel = mongoose.model("User", userSchema);
+module.exports = userModel
