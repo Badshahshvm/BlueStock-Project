d656fb3236e8a6a991f64e20d25960a60549e234
Shivam Kumar <115875580+Badshahshvm@users.noreply.github.com>
2025-01-16 17:07:44 +0530

    Update Ipo.js

--- backend/model/Ipo.js
+++ backend/model/Ipo.js
@@ -1,6 +1,64 @@
-  const mongoose=require("mongoose");
-const ipoSchema=new mongoose.Schema({
-})
+const mongoose = require('mongoose');
 
-const ipoModel=mongoose.model("Ipo",ipoSchema);
-module.exports=ipoModel;
+const ipoSchema = new mongoose.Schema({
+              companyLogoUrl: {
+                            type: String,
+                            required: true,
+                            default: ""
+              },
+              companyName: {
+                            type: String,
+                            required: true,
+              },
+              price_band: {
+                            type: String,
+                            required: true,
+              },
+              open: {
+                            type: Date,
+                            required: true,
+              },
+              close: {
+                            type: Date,
+                            required: true,
+              },
+              issue_size: {
+                            type: Number,
+                            required: true,
+              },
+              listing_date: {
+                            type: Date,
+              },
+              status: {
+                            type: String,
+                            enum: ["Open", "Closed", "Listed", "Upcoming"],
+                            required: true,
+              },
+              ipo_price: {
+                            type: Number,
+                            required: true,
+              },
+              listing_price: {
+                            type: Number,
+              },
+              listing_gain: {
+                            type: String,
+              },
+              cmp: {
+                            type: Number,
+              },
+              current_return: {
+                            type: String,
+              },
+              rhp: {
+                            type: String,
+              },
+              drhp: {
+                            type: String,
+              },
+}, {
+              timestamps: true
+});
+const ipoModel = mongoose.model('IPO', ipoSchema);
+module.exports = ipoModel
+
